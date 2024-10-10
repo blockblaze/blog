37b636804800437d478126aa9e1a22acfdd1d0da
blockblaze <blockblaze12@gmail.com>
2024-10-10 23:21:48 +0300

    Connect to the mysql database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+//handling db connection
+const dbconnection = mysql.createConnection({
+host: process.env.HOST,
+user: process.env.USER,
+password: process.env.PASSWORD,
+database: process.env.DATABASE,
+});
+dbconnection.connect();
+  
+
 app.get("/",function(req,res){
-    res.status(200).json({"message":"work"})
+    dbconnection.query("SELECT * FROM posts WHERE post_id = 4",function(result,err){
+        if(err) return res.status(500).send(err);
+        res.status(200).json(result)
+        
+})
 })
 
 // Start the server
@@ -15,3 +33,19 @@ app.listen(port, (error) => {
       console.log(`Server running on port ${port}`);
     }
   });
+
+
+// Close the dbconnection gracefully when the process is terminated
+const gracefulShutdown = () => {
+dbconnection.end((err) => {
+if (err) {
+console.error("Error closing the database connection:", err);
+} else {
+console.log("Database connection closed");
+}
+process.exit();
+});
+};
+
+process.on("SIGINT", gracefulShutdown);
+process.on("SIGTERM", gracefulShutdown);
