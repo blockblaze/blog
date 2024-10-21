bd912b86d833b3d4191f8855b662f4a83a5b1456
blockblaze <blockblaze12@gmail.com>
2024-10-21 03:04:56 +0300

    Complete upload post image functionality

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -1,62 +1,143 @@
 import { FileInput, Select, TextInput } from "flowbite-react";
 import { useState } from "react";
-import CKEditorCom from "./CKEditor"
+import CKEditorCom from "./CKEditor";
 // import ReactQuill, {  } from 'react-quill'; // Import Quill here
 // import 'react-quill/dist/quill.snow.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+
 
 export function CreatePost() {
-    const [formValue, setFormValue] = useState({});
-    const [category , setCategory] = useState('');
+  const [files, setFiles] = useState({});
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const [category, setCategory] = useState("");
 
-    console.log(formValue)
-
-    function handleEditorChange(data){
-        setFormValue({...formValue , content:data})
+  console.log(formData)
+  const handleUploadImage = async () => {
+    if (!files.image) {
+      setImageUploadError("Please, select an Image.");
+      return;
     }
 
-    return (
-        <div className="p-3 w-full max-w-3xl mx-auto min-h-screen">
-            <h1 className="text-center text-4xl my-7 font-bold">Create a post</h1>
-            <form className="flex flex-col gap-4">
-                <div className="flex flex-col gap-4 sm:flex-row justify-between">
-                    <TextInput type="text" placeholder="Title" id="title" className="flex-1" required />
-                    <Select required onChange={(e)=>{setCategory(e.target.value)}}>
-                        <option value="">Select a category</option>
-                        <option value="maps">Maps</option>
-                        <option value="scripts">Scripts</option>
-                        <option value="articles">Articles</option>
-                    </Select>
-                </div>
-                <TextInput type="text" placeholder="Slug" id="slug" required />
+    const image = files.image;
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + "-" + image.name;
+    const storageRef = ref(storage, `images/${fileName}`);
+    const uploadTask = uploadBytesResumable(storageRef, image);
 
-                <div className="flex gap-4 items-center justify-between border-4 border-custom-dark-orange border-dotted p-3">
-                    <FileInput type="file" accept="images/*" />
-                    <button className="bg-custom-orange text-white p-2 m:p-3 rounded hover:bg-custom-dark-orange font-medium">
-                        Upload image
-                    </button>
-                </div>
-
-                {/* Quill Editor */}
-                {/* <ReactQuill 
-                    theme="snow" 
-                    placeholder="Enter something..." 
-                    className="h-72 mb-12" 
-                    value={editorValue} 
-                    onChange={setEditorValue}
-                /> */}
-                <CKEditorCom 
-                setEditorValue={handleEditorChange}
-                />
-
-                {/* Downloadables */}
-
-                {category === "maps" || category ==="scripts"?                 <><TextInput type="text" placeholder="Supported Minecraft version" id="supported-version" required /><FileInput type="file" accept="images/*" /></>
-                : null}
-
-                    <button className="bg-custom-orange text-white p-2 m:p-3 rounded hover:bg-custom-dark-orange font-medium">
-                        Create
-                    </button>
-            </form>
-        </div>
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageUploadError('Image upload failed');
+        setImageUploadProgress(null);
+      },
+      () => {
+        // Get download URL after successful upload
+        setImageUploadError(null);
+        setImageUploadProgress(null);
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          setFormData({ ...formData, imageUrl: url });
+        });
+      }
     );
+  };
+
+  const handleEditorChange = (data) => {
+    setFormData({ ...formData, content: data });
+  };
+
+  return (
+    <div className="p-3 w-full max-w-3xl mx-auto min-h-screen">
+      <h1 className="text-center text-4xl my-7 font-bold">Create a post</h1>
+      <form className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 sm:flex-row justify-between">
+          <TextInput
+            type="text"
+            placeholder="Title"
+            id="title"
+            className="flex-1"
+            required
+          />
+          <Select
+            
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
+            required
+          >
+            <option value="">Select a category</option>
+            <option value="maps">Maps</option>
+            <option value="scripts">Scripts</option>
+            <option value="articles">Articles</option>
+          </Select>
+        </div>
+        <TextInput type="text" placeholder="Slug" id="slug"  required/>
+
+        <div className="flex gap-4 items-center justify-between border-4 border-custom-dark-orange border-dotted p-3">
+          <FileInput
+            type="file"
+            accept="images/*"
+            onChange={(e) => {
+              setFiles({ ...files, image: e.target.files[0] });
+            }}
+            required
+          />
+            <button className="bg-custom-orange text-white p-2 m:p-3 rounded hover:bg-custom-dark-orange font-medium" type="button" onClick={handleUploadImage}>
+            {
+                imageUploadProgress?<div className="w-16 h-16">
+                     <CircularProgressbar
+                  value={imageUploadProgress}
+                  text={`${imageUploadProgress || 0}%`}
+                />
+                </div>:'Upload Image'
+            }
+          </button>
+
+        </div>
+
+        {/* Quill Editor */}
+        {/* <ReactQuill 
+                        theme="snow" 
+                        placeholder="Enter something..." 
+                        className="h-72 mb-12" 
+                        value={editorValue} 
+                        onChange={setEditorValue}
+                    /> */}
+        <CKEditorCom setEditorValue={handleEditorChange} />
+
+        {/* Downloadables */}
+
+        {category === "maps" || category === "scripts" ? (
+          <>
+            <TextInput
+              type="text"
+              placeholder="Supported Minecraft version"
+              id="supported-version"
+              required
+            />
+            <FileInput type="file" accept="images/*" required/>
+          </>
+        ) : null}
+
+        <button className="bg-custom-orange text-white p-2 m:p-3 rounded hover:bg-custom-dark-orange font-medium">
+          Create
+        </button>
+      </form>
+    </div>
+  );
 }
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,8 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // eslint-disable-next-line no-undef
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "personal-blog-8ea87.firebaseapp.com",
   projectId: "personal-blog-8ea87",
   storageBucket: "personal-blog-8ea87.appspot.com",
