2b4b78a57d8d74e918e5486b7d61ccc021dc6581
blockblaze <blockblaze12@gmail.com>
2024-12-14 00:31:26 +0200

    Fix some errors for deployment

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
-const flowbite = require("flowbite-react/tailwind");
+import flowbite from "flowbite-react/tailwind";
+
 
 /** @type {import('tailwindcss').Config} */
 export default {
